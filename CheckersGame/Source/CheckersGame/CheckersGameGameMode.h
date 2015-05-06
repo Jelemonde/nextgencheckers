439fd1b4eb28bc63862ad39b0a9768ececb6437e
FuncDetail <jeremielemonde@gmail.com>
2015-05-05 23:30:04 -0400

    Test Commit

---
+++ CheckersGame/Source/CheckersGame/CheckersGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CheckersGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHECKERSGAME_API ACheckersGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
